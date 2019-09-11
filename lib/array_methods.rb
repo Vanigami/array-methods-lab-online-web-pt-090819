bb2d9d1cf8a41a95b1cbf1192bea142570af16a5
Vanna Winland <vgwinland@gmail.com>
2019-09-11 21:28:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,16 +3,16 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
 
-end
+end 
 
 def using_last(array)
 
